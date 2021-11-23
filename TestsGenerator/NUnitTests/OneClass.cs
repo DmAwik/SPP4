95a99dfba207be2cabf890d1995db76155a3650c
DmAwik <Awii15@mail.ru>
2021-11-24 02:37:42 +0300

    add tests

---
+++ TestsGenerator/NUnitTests/OneClass.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace NUnitTests
+{
+    public class ThirdClass
+    {
+        public void Hi() 
+        {
+            Console.WriteLine("Hi!");
+        }
+    }    
+}
