71b4f3f0ef2ade43e441b2980a8e4d6f42223558
DmAwik <Awii15@mail.ru>
2021-11-24 02:36:25 +0300

    Add TestsGenerator logic

---
+++ TestsGenerator/TestsGenerator/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Threading.Tasks;
+
+namespace TestsGenerator
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            string FolderPath = "C:\\Users\\userD\\Desktop\\SPP4\\resource";
+
+            List<string> FilesPath = new List<string>() 
+            {
+                "C:\\Users\\userD\\Desktop\\SPP4\\TestsGenerator\\NUnitTests\\OneClass.cs",
+                "C:\\Users\\userD\\Desktop\\SPP4\\TestsGenerator\\NUnitTests\\TwoClasses.cs"
+            };
+
+            Pipeline p = new Pipeline(new PipelineConfiguration(1, 1, 1));
+            await p.Execute(FilesPath, FolderPath);
+            Console.ReadKey();
+        }
+    }
+}
